29f93ae2a4f22c370ec02b7033f26c5d7978da66
gayatrisinha <gayatrisinha23@gmail.com>
2018-06-12 11:43:17 +0530

    Added some logic

--- src/Framework/FirstCode.java
+++ src/Framework/FirstCode.java
@@ -4,6 +4,9 @@ public class FirstCode {
 	public void someMethod() {
 		System.out.println("somemethod");
 	}
+	public void branchMethod() {
+		System.out.println("branchmethod");
+	}
 
 	public static void main(String[] args) {
 		FirstCode fs=new FirstCode();
